a47a0b523c721e45bd00d97e79e8dea32b0f8a2d
CagesThrottleUs <manstein.felix@gmail.com>
2026-09-26 10:56:25 +0530

    refactor: add support for clang-tidy
    This ensures that static analysis of code uses best possible guides and
    solutions for C++.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon
 // src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-int main() {
+auto main() -> int {
   // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
   // <b>lang</b> variable name to see how CLion can help you rename it.
 
-  const auto lang = "C++";
-  std::cout << "Hello and welcome to " << lang << "!\n";
+  const auto* const LANG = "C++";
+  std::cout << "Hello and welcome to " << LANG << "!\n";
 
-  for (int i = 1; i <= 5; i++) {
+  constexpr int LOOP_SIZE = 5;
+
+  for (int i = 1; i <= LOOP_SIZE; i++) {
     // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We
     // have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
     // for you, but you can always add more by pressing <shortcut
     // actionId="ToggleLineBreakpoint"/>.
-    std::cout << "i = " << i << std::endl;
+    std::cout << "i = " << i << '\n';
   }
 
   return 0;
